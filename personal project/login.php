be13616d21bfe87a2f193230fcf408cef4db2570
Sara Simnica <Sarasimnica@Outlook.com>
2025-10-22 23:15:54 +0200

    update

--- personal project/login.php
+++ personal project/login.php
@@ -1,3 +1,25 @@
+<style>
+		
+  table
+  {
+    border: 1px solid black;
+  }
+
+  tr,td,th
+  {
+    border: 1px solid black;
+    
+  }
+  table,tr,td
+  {
+    border-collapse: collapse;
+  }
+  td
+  {
+    padding: 10px;
+  }
+
+</style>
 
 <!DOCTYPE html>
 <html lang="en">
